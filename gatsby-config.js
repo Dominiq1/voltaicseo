e3590d317b97af4893de08ef842ff6b544500b2a
Dominiq Martinez <dominiqmartinez13@gmail.com>
2024-01-30 10:36:19 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `voltaicseo`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-google-gtag"]
+};
